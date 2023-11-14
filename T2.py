ddc4b9f17c43a3fae56e3399e526384c0abe8118
Shaïna <91091643+28shaina@users.noreply.github.com>
2023-11-13 19:47:44 -0500

    T2 code en python

---
+++ T2.py
@@ -0,0 +1,15 @@
+import pandas as pd
+from scipy.stats import linregress
+
+# Lire les données
+fichier = pd.read_csv('jfreechart-test-stats.csv')
+
+# Régression linéaire pour TLOC et TASSERT
+slope, intercept, r_value, p_value, std_err = linregress(fichier['TLOC'], fichier[' TASSERT'])
+print(f'Régression linéaire pour TLOC et TASSERT: pente = {slope}, intercept = {intercept}, r(Pearson) = {r_value}')
+
+
+
+# Régression linéaire pour WMC et TASSERT
+slope, intercept, r_value, p_value, std_err = linregress(fichier[' WMC'], fichier[' TASSERT'])
+print(f'Régression linéaire pour WMC et TASSERT: pente = {slope}, intercept = {intercept}, r(Pearson) = {r_value}')
